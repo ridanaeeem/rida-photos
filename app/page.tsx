fdca90e6d8e0eb336e9627f7ec5a8762fc895da1
ridanaeeem <ridan@bu.edu>
2024-08-31 20:17:35 -0400

    framer motion test

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 import Image from "next/image";
+import { motion } from "framer-motion";
+
 export default function Home() {
 	return (
 		<div>
@@ -9,20 +11,46 @@ export default function Home() {
 					<h1 className="text-4xl">Rida Naeem Photography</h1>
 					<h2 className="text-2xl">Memories</h2>
 				</div>
-				<div className="grid grid-cols-3 gap-4 p-5">
-					<div className="col-start-2 col-span-2">
-						<img src="/waterLily.JPG" alt="water lily"></img>
+				<motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }}>
+					<div className="grid grid-cols-3 gap-4 p-5">
+						<div className="col-start-2 col-span-2">
+							<img src="/waterLily.JPG" alt="water lily"></img>
+						</div>
 					</div>
-				</div>
-				<div className="grid grid-cols-4 grid-rows-3 gap-4 px-5">
-					<div className="col-span-2 row-start-1 row-span-2">
-						<img src="/fireworks.JPG" alt="water lily"></img>
+				</motion.div>
+				<motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }}>
+					<div className="grid grid-cols-4 grid-rows-3 gap-4 px-5">
+						<div className="col-span-2 row-start-1 row-span-2">
+							<img src="/fireworks.JPG" alt="water lily"></img>
+						</div>
+						<div className="col-span-2 row-start-2 row-span-2">
+							<img src="/chicagoEyes.JPG" alt="water lily"></img>
+						</div>
 					</div>
-					<div className="col-span-2 row-start-2 row-span-2">
-						<img src="/chicagoEyes.JPG" alt="water lily"></img>
+				</motion.div>
+				<motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }}>
+					<img className="p-5" src="/haSunset.JPG" alt="water lily"></img>
+				</motion.div>
+				<motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }}>
+					<div className="grid grid-cols-3 gap-4 p-5">
+						<div className="col-start-2 col-span-2">
+							<img src="/waterLily.JPG" alt="water lily"></img>
+						</div>
 					</div>
-				</div>
-				<img className="p-5" src="/haSunset.JPG" alt="water lily"></img>
+				</motion.div>
+				<motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }}>
+					<div className="grid grid-cols-4 grid-rows-3 gap-4 px-5">
+						<div className="col-span-2 row-start-1 row-span-2">
+							<img src="/fireworks.JPG" alt="water lily"></img>
+						</div>
+						<div className="col-span-2 row-start-2 row-span-2">
+							<img src="/chicagoEyes.JPG" alt="water lily"></img>
+						</div>
+					</div>
+				</motion.div>
+				<motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }}>
+					<img className="p-5" src="/haSunset.JPG" alt="water lily"></img>
+				</motion.div>
 			</main>
 		</div>
 	);
