4dc4c3fc65797b08e18bc3fe569b5ed7c5dec2d5
ridanaeeem <ridan@bu.edu>
2024-08-31 20:08:39 -0400

    testing photos

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
+"use client";
 import Image from "next/image";
-
 export default function Home() {
 	return (
 		<div>
@@ -9,7 +9,20 @@ export default function Home() {
 					<h1 className="text-4xl">Rida Naeem Photography</h1>
 					<h2 className="text-2xl">Memories</h2>
 				</div>
-        <div></div>
+				<div className="grid grid-cols-3 gap-4 p-5">
+					<div className="col-start-2 col-span-2">
+						<img src="/waterLily.JPG" alt="water lily"></img>
+					</div>
+				</div>
+				<div className="grid grid-cols-4 grid-rows-3 gap-4 px-5">
+					<div className="col-span-2 row-start-1 row-span-2">
+						<img src="/fireworks.JPG" alt="water lily"></img>
+					</div>
+					<div className="col-span-2 row-start-2 row-span-2">
+						<img src="/chicagoEyes.JPG" alt="water lily"></img>
+					</div>
+				</div>
+				<img className="p-5" src="/haSunset.JPG" alt="water lily"></img>
 			</main>
 		</div>
 	);
