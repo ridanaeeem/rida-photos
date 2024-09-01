cb7bcc1b092a7c8693a1ba6bfb7b1a530cdce18a
ridanaeeem <ridan@bu.edu>
2024-09-01 04:32:22 -0400

    font and background

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 import PhotoThumbnail from "@/components/PhotoThumbnail";
 import PhotoModal from "@/components/PhotoModal";
+import { Caveat } from "next/font/google";
+
+const caveat = Caveat({ subsets: ["latin"] });
 
 interface ModalData {
 	isOpen: boolean;
@@ -40,91 +43,92 @@ export default function Home() {
 	return (
 		<div>
 			<title>Rida Naeem Photography</title>
-			<main>
-				<div className="header flex flex-col p-5">
-					<h1 className="text-4xl">Rida Naeem Photography</h1>
-					<h2 className="text-2xl">Memories</h2>
-				</div>
-
-				<PhotoModal
-					isOpen={modalData.isOpen}
-					imageSrc={modalData.imageSrc}
-					imageAlt={modalData.imageAlt}
-					description={modalData.description}
-				/>
-
-				{modalData.isOpen ? (
-					<div className="fixed inset-0 z-10 bg-black bg-opacity-75" onClick={() => closeModal()}></div>
-				) : null}
-
-				<motion.div
-					initial={{ opacity: 0 }}
-					whileInView={{ opacity: 1 }}
-					transition={{
-						duration: transitionLength,
-					}}>
-					<div className="grid grid-cols-3 gap-4 p-5 lg:grid-cols-6">
-						<div className="col-start-2 col-span-2 lg:col-start-4">
-							<PhotoThumbnail
-								setModalData={setModalData}
-								src="/waterLily.JPG"
-								alt="water lily"
-								description="water lily"
-								camera="SONY DSC-RX100M6"
-								stats="f/11.0 • 1/4000 • 16.21mm • ISO12800"
-							/>
-						</div>
+			<main className={caveat.className}>
+				<div className="bg-[#9EB8CF]">
+					<div className="header flex flex-col p-5">
+						<h1 className="text-4xl">Rida Naeem Photography</h1>
+						<h2 className="text-2xl">Memories</h2>
 					</div>
-				</motion.div>
 
-				<motion.div
-					initial={{ opacity: 0 }}
-					whileInView={{ opacity: 1 }}
-					transition={{
-						duration: transitionLength,
-					}}>
-					<div className="grid grid-cols-4 grid-rows-3 gap-4 px-5">
-						<div className="col-span-2 row-start-1 row-span-2">
-							<PhotoThumbnail
-								setModalData={setModalData}
-								src="/fireworks.JPG"
-								alt="fireworks"
-								description="fireworks"
-								camera="SONY DSC-RX100M6"
-								stats="f/11.0 • 1/4000 • 16.21mm • ISO12800"
-							/>
-						</div>
-						<div className="col-span-2 row-start-2 row-span-2">
-							<PhotoThumbnail
-								setModalData={setModalData}
-								src="/chicagoEyes.JPG"
-								alt="Chicago Eyes"
-								description="Chicago Eyes"
-								camera="SONY DSC-RX100M6"
-								stats="f/11.0 • 1/4000 • 16.21mm • ISO12800"
-							/>
-						</div>
-					</div>
-				</motion.div>
-
-				<motion.div
-					initial={{ opacity: 0 }}
-					whileInView={{ opacity: 1 }}
-					transition={{
-						duration: transitionLength,
-					}}
-					className="p-5">
-					<PhotoThumbnail
-						setModalData={setModalData}
-						src="/haSunset.JPG"
-						alt="Sunset"
-						description="Sunset"
-						camera="SONY DSC-RX100M6"
-						stats="f/11.0 • 1/4000 • 16.21mm • ISO12800"
+					<PhotoModal
+						isOpen={modalData.isOpen}
+						imageSrc={modalData.imageSrc}
+						imageAlt={modalData.imageAlt}
+						description={modalData.description}
 					/>
-				</motion.div>
+
+					{modalData.isOpen ? (
+						<div className="fixed inset-0 z-10 bg-black bg-opacity-75" onClick={() => closeModal()}></div>
+					) : null}
+
+					<motion.div
+						initial={{ opacity: 0 }}
+						whileInView={{ opacity: 1 }}
+						transition={{
+							duration: transitionLength,
+						}}>
+						<div className="grid grid-cols-3 gap-4 p-5 lg:grid-cols-6">
+							<div className="col-start-2 col-span-2 lg:col-start-4">
+								<PhotoThumbnail
+									setModalData={setModalData}
+									src="/waterLily.JPG"
+									alt="water lily"
+									description="water lily"
+									camera="SONY DSC-RX100M6"
+									stats="f/11.0 • 1/4000 • 16.21mm • ISO12800"
+								/>
+							</div>
+						</div>
+					</motion.div>
+
+					<motion.div
+						initial={{ opacity: 0 }}
+						whileInView={{ opacity: 1 }}
+						transition={{
+							duration: transitionLength,
+						}}>
+						<div className="grid grid-cols-4 grid-rows-3 gap-4 px-5">
+							<div className="col-span-2 row-start-1 row-span-2">
+								<PhotoThumbnail
+									setModalData={setModalData}
+									src="/fireworks.JPG"
+									alt="fireworks"
+									description="fireworks"
+									camera="SONY DSC-RX100M6"
+									stats="f/11.0 • 1/4000 • 16.21mm • ISO12800"
+								/>
+							</div>
+							<div className="col-span-2 row-start-2 row-span-2">
+								<PhotoThumbnail
+									setModalData={setModalData}
+									src="/chicagoEyes.JPG"
+									alt="Chicago Eyes"
+									description="Chicago Eyes"
+									camera="SONY DSC-RX100M6"
+									stats="f/11.0 • 1/4000 • 16.21mm • ISO12800"
+								/>
+							</div>
+						</div>
+					</motion.div>
+
+					<motion.div
+						initial={{ opacity: 0 }}
+						whileInView={{ opacity: 1 }}
+						transition={{
+							duration: transitionLength,
+						}}
+						className="p-5">
+						<PhotoThumbnail
+							setModalData={setModalData}
+							src="/haSunset.JPG"
+							alt="Sunset"
+							description="Sunset"
+							camera="SONY DSC-RX100M6"
+							stats="f/11.0 • 1/4000 • 16.21mm • ISO12800"
+						/>
+					</motion.div>
+				</div>
 			</main>
 		</div>
 	);
 }
-
