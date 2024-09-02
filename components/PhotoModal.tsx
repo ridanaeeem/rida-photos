25c317b446fe544579daa34f82661fb61bf5bc52
ridanaeeem <ridan@bu.edu>
2024-09-02 12:59:16 -0400

    border fix

--- components/PhotoModal.tsx
+++ components/PhotoModal.tsx
@@ -14,8 +14,8 @@ const PhotoModal: React.FC<PhotoModalProps> = ({ isOpen, imageSrc, imageAlt, des
 	if (!isOpen) return null;
 
 	return (
-		<div className="fixed z-50 flex items-center justify-center m-5 lg:m-auto text-center">
-			<div className="relative bg-white p-3 md:p-5 rounded-lg">
+		<div className="fixed lg:inset-0 z-50 flex items-center justify-center m-5 lg:m-[20rem] text-center">
+			<div className="fixed  relative bg-white p-3 md:p-5 rounded-lg">
 				{/* <button
 					className="absolute p-2 top-2 right-1 text-2xl text-gray-500 hover:text-gray-800"
 					onClick={onClose}>
