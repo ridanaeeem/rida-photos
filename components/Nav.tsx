938b55d885a5774077a5f6bb412a1d36d9753533
ridanaeeem <ridan@bu.edu>
2024-09-06 11:23:07 -0400

    header colors

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -6,20 +6,20 @@ export default function Nav() {
 	const pathname = usePathname();
 
 	return (
-		<nav>
-			<div className="header flex flex-col p-2 md:p-5 bg-white">
-				<h1 className={`text-4xl ${pathname === "/" ? "text-[#809BB3]" : "text-black"}`}>
+		<nav className={`${pathname === "/collections" ? "bg-[#171719] text-white" : "bg-white text-black"}`}>
+			<div className="header flex flex-col p-2 md:p-5">
+				<h1 className={`text-4xl ${pathname === "/" ? "text-[#809BB3]" : null}`}>
 					<Link href="/">Rida Naeem Photography</Link>
 				</h1>
 			</div>
 			<ul className="text-xl md:text-3xl flex justify-center pb-5">
-				<ul className={`px-5 ${pathname === "/collections" ? "text-[#809BB3]" : "text-black"}`}>
+				<ul className={`px-5 ${pathname === "/collections" ? "text-[#809BB3]" : "null"}`}>
 					<Link href="/collections">Collections</Link>
 				</ul>
-				<ul className={`px-5 ${pathname === "/contact" ? "text-[#809BB3]" : "text-black"}`}>
+				<ul className={`px-5 ${pathname === "/contact" ? "text-[#809BB3]" : "null"}`}>
 					<Link href="/contact">Contact</Link>
 				</ul>
-				<ul className={`px-5 ${pathname === "/about" ? "text-[#809BB3]" : "text-black"}`}>
+				<ul className={`px-5 ${pathname === "/about" ? "text-[#809BB3]" : "null"}`}>
 					<Link href="/about">About</Link>
 				</ul>
 			</ul>
