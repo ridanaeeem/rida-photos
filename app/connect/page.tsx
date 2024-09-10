84f145826e381bf260bb9c2e31b817f88fd8f0f6
ridanaeeem <ridan@bu.edu>
2024-09-10 04:00:11 -0400

    fonts

--- app/connect/page.tsx
+++ app/connect/page.tsx
@@ -5,50 +5,47 @@ export default function Contact() {
 		<div>
 			<title>Contact</title>
 			<main>
-				<div className="bg-gradient-to-b from-white to-[#9EB8CF]">
-					<div className="min-h-screen">
-						<h1 className="text-4xl text-left font-bold p-6">About Me</h1>
-						<p className="text-lg md:text-2xl text-left px-6">
-							Hi! My name is Rida, and I recently graduated from Boston University where I studied
-							Neuroscience and Computer Science. If you want to learn more about my work in those fields,
-							check out{" "}
-							<a href="https://rnaeem.com" target="_blank" className="text-blue-600">
-								my portfolio
-							</a>
-							! Otherwise feel free to explore my photography here. I have been taking photos ever since I
-							was a little kid and had a{" "}
-							<a
-								href="https://www.amazon.com/VTech-Kidizoom-Camera-2010-Version/dp/B0027CS7D6"
-								target="_blank"
-								className="text-blue-600">
-								VTech Kidizoom Camera
-							</a>
-							. These days I tend to use a Sony RX100M6 and FujiFilm X100F, but this camera will forever
-							be important to me. My photos mostly capture moments in my life that I want to preserve, and
-							I hope that viewing them conveys the same emotions I felt when taking them. Take a look on
-							the{" "}
-							<a href="/" className="text-blue-600">
-								main page
-							</a>{" "}
-							to scroll through some of my favorite photos, and make sure to check out the{" "}
-							<a href="/collections" className="text-blue-600">
-								Collections page
-							</a>
-							to see more specific themes. Enjoy!
-						</p>
-						<h1 className="text-4xl text-left font-bold p-6">Contact Me</h1>
-						<p className="text-lg md:text-2xl text-left px-6 pb-6">
-							Email:{" "}
-							<a href="mailto:ridan@bu.edu" className="text-blue-600">
-								ridan@bu.edu
-							</a>
-							<br></br>
-							Instagram{" "}
-							<a href="https://www.instagram.com/ridasphotos/" className="text-blue-600">
-								@ridasphotos
-							</a>
-						</p>
-					</div>
+				<div className="bg-gradient-to-b from-[#171719] to-[#171719] min-h-screen text-white">
+					<h1 className="text-4xl text-left font-bold p-6">About Me</h1>
+					<p className="text-lg md:text-2xl text-left px-6">
+						Hi! My name is Rida, and I recently graduated from Boston University where I studied
+						Neuroscience and Computer Science. If you want to learn more about my work in those fields,
+						check out{" "}
+						<a href="https://rnaeem.com" target="_blank" className="text-blue-600">
+							my portfolio
+						</a>
+						! Otherwise feel free to explore my photography here. I have been taking photos ever since I was
+						a little kid and had a{" "}
+						<a
+							href="https://www.amazon.com/VTech-Kidizoom-Camera-2010-Version/dp/B0027CS7D6"
+							target="_blank"
+							className="text-blue-600">
+							VTech Kidizoom Camera
+						</a>
+						. These days I tend to use a Sony RX100M6 and FujiFilm X100F, but this camera will forever be
+						important to me. My photos mostly capture moments in my life that I want to preserve, and I hope
+						that viewing them conveys the same emotions I felt when taking them. Take a look on the{" "}
+						<a href="/" className="text-blue-600">
+							main page
+						</a>{" "}
+						to scroll through some of my favorite photos, and make sure to check out the{" "}
+						<a href="/collections" className="text-blue-600">
+							Collections page
+						</a>
+						to see more specific themes. Enjoy!
+					</p>
+					<h1 className="text-4xl text-left font-bold p-6">Contact Me</h1>
+					<p className="text-lg md:text-2xl text-left px-6 pb-6">
+						Email:{" "}
+						<a href="mailto:ridan@bu.edu" className="text-blue-600">
+							ridan@bu.edu
+						</a>
+						<br></br>
+						Instagram{" "}
+						<a href="https://www.instagram.com/ridasphotos/" className="text-blue-600">
+							@ridasphotos
+						</a>
+					</p>
 				</div>
 			</main>
 		</div>
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,7 +2,9 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Caveat } from "next/font/google";
+import { Darker_Grotesque } from "next/font/google";
 
+const darker_grotesque = Darker_Grotesque({ subsets: ["latin"] });
 const caveat = Caveat({ subsets: ["latin"] });
 
 export default function Nav() {
@@ -10,15 +12,15 @@ export default function Nav() {
 
 	return (
 		<nav
-			className={`${caveat.className} ${
-				pathname === "/collections" ? "bg-[#171719] text-white" : "bg-white text-black"
+			className={`${darker_grotesque.className} ${
+				pathname === "/" ? "bg-white text-black" : "bg-[#171719] text-white"
 			}`}>
-			<div className="header flex flex-col p-2 md:p-5">
-				<h1 className={`text-4xl ${pathname === "/" ? "text-[#809BB3]" : null}`}>
+			<div className="header flex flex-col font-bold p-2 md:p-5">
+				<h1 className={`text-4xl ${caveat.className} ${pathname === "/" ? "text-[#809BB3]" : null}`}>
 					<Link href="/">Rida Naeem Photography</Link>
 				</h1>
 			</div>
-			<ul className="text-xl md:text-3xl flex justify-center pb-5">
+			<ul className="text-xl md:text-3xl flex justify-center pb-5 font-normal">
 				<ul className={`px-5 ${pathname === "/collections" ? "text-[#809BB3]" : "null"}`}>
 					<Link href="/collections">Collections</Link>
 				</ul>
